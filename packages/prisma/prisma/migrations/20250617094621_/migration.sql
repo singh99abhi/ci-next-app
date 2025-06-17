d42ce2549c03efda8123e4acc6d7135eabd48c0d
Abhishek Kumar Singh <abhishekkumar.singh@copart.com>
2025-06-17 05:19:47 -0500

    added prisma

---
+++ packages/prisma/prisma/migrations/20250617094621_/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - The required column `id` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
