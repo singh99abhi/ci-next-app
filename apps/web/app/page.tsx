159ae7f21885588b6b0c444e0631f15898b95e9a
singh99abhi <singha78059@gmail.com>
2025-06-18 10:42:53 -0500

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const user=await client.user.findFirst()
   return (
     <div>
-        First name:
+        first name:
         {user?.username} 
         password:
         {user?.password }
